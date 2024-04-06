35cc770a18b53f68d2563a5d59d89decaf0813d3
Trịnh Khánh <realkripper@gmail.com>
2024-04-06 08:00:26 +0700

    Update import

--- src/pages/guest/Home.page.tsx
+++ src/pages/guest/Home.page.tsx
@@ -73,9 +73,12 @@ export function Quote() {
         <Text fontSize="2xl" mb={4}>
           Push <em>harder</em> than yesterday<br /> if you want a <em>different</em> tomorrow.
         </Text>
-        <Button bg="#426B1F" color="#FFF" variant="solid" size="lg" _hover={{ bg: "#e0e0e0", color: "#426B1F" }}>
-          Browse Newsfeed
-        </Button>
+        <Link href='/Newsfeed'>
+          <Button bg="#426B1F" color="#FFF" variant="solid" size="lg" _hover={{ bg: "#e0e0e0", color: "#426B1F" }}>
+            Browse Newsfeed
+          </Button>
+        </Link>
+
       </Center>
     </Box>
   );
