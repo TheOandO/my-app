49e27b0222c24e643fb729057eb28644db3f472b
RileyReidx3 <realkripper@gmail.com>
2024-04-20 04:19:37 +0700

    delete backend

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const authRouter = require("./auth")
-const userRouter = require("./user")
-exports.routerIndex = (app)=>{ 
-    app.use("/auth", authRouter);
-    app.use("/user", userRouter)
-   
-}
