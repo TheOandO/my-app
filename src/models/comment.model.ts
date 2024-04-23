9e3e928b26bfdd74fd6abb3a6669dc33fb307461
nguyentien179 <88731661+nguyentien179@users.noreply.github.com>
2024-04-24 03:23:42 +0700

    changes for mc

--- src/models/comment.model.ts
+++ src/models/comment.model.ts
@@ -1,33 +1,21 @@
 import { Schema, model } from "mongoose";
 import { roles } from "../utils";
 
-interface IComment{
-    text: string;
-    user_id: Schema.Types.ObjectId;
-    article_id: Schema.Types.ObjectId;
+interface IComment {
+  text: string;
 }
 
-const CommentSchema = new Schema<IComment>({
+const CommentSchema = new Schema<IComment>(
+  {
     text: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
-    user_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        trim: true,
-        
-    },
-    article_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Article',
-        required: true,
-        trim: true,
-    }
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-export const Comment = model<IComment>('Comment', CommentSchema);
+export const Comment = model<IComment>("Comment", CommentSchema);
