496ab6cd538fc1f1b3a54f0a33e1861fca4c411b
RileyReidx3 <realkripper@gmail.com>
2024-05-02 06:50:21 +0700

    Search function MM complete

--- src/pages/marketing manager/MMMonitor.page.tsx
+++ src/pages/marketing manager/MMMonitor.page.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, Link, VStack, Text, Flex, Image, Divider, Heading, Icon, Input, InputGroup, Select, Table, Tbody, Td, Th, Thead, Tr, useColorModeValue, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, HStack, Tag, useToast, InputLeftElement, Avatar } from "@chakra-ui/react";
-import { FaNewspaper, FaSearch, FaSortAmountDown, FaSortAmountUp } from "react-icons/fa";
+import { Box, Button, Link, VStack, Text, Flex, Image, Divider, Heading, Input, InputGroup, Table, Tbody, Td, Th, Thead, Tr, useColorModeValue, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, HStack, Tag, useToast, InputLeftElement, Avatar, Alert, AlertIcon } from "@chakra-ui/react";
+import { FaNewspaper, FaSearch } from "react-icons/fa";
 import { useLocation } from "react-router-dom";
 import { Link as RouterLink } from "react-router-dom";
 import { LoggedinHeader } from "../admin/AdminHome.page";
@@ -33,7 +33,6 @@ export function MMSidebar() {
     );
 }
 
-
 interface User {
   _id: string;
   name: string;
@@ -44,50 +43,6 @@ interface User {
   username: string;
   createdAt: string;
 }
-//   // Example members data
-//   const members = [
-//     {
-//         id: 'P0001',
-//         name: 'Nicky Nicknack',
-//         email: 'nick@website.edu',
-//         role: 'Student',
-//         major: 'Photography',
-//         faculty: 'Faculty A',
-//     },
-//     {
-//         id: 'CS001',
-//         name: 'Mandy Chow',
-//         email: 'mandy@website.edu',
-//         role: 'Student',
-//         major: 'Computer Science',
-//         faculty: 'Faculty A',
-//     },
-//     {
-//         id: 'CS002',
-//         name: 'Kaling Bling',
-//         email: 'kaling@website.edu',
-//         role: 'Student',
-//         major: 'Computer Science',
-//         faculty: 'Faculty B',
-//     },
-//     {
-//       id: 'MC001',
-//       name: 'Super',
-//       email: 'sober@website.edu',
-//       role: 'Marketing Coordinator',
-//       major: 'None',
-//       faculty: 'Faculty A',
-//     },
-//     {
-//       id: 'MC002',
-//       name: 'Bat',
-//       email: 'bet@website.edu',
-//       role: 'Marketing Coordinator',
-//       major: 'None',
-//       faculty: 'Faculty B',
-//     },
-// ];
-
 interface Entry {
   _id: string;
   name: string;
@@ -116,82 +71,29 @@ interface Comment {
   article_id: string
 }
 
-type StatusType = 'Waiting' | 'Rejected' | 'Overdue' | 'Published';
-//   const pendingArticles = [
-//     {
-//       id: 1,
-//       title: 'No alarms to no surprises',
-//       summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae mauris eu ex tincidunt scelerisque vel et risus. Fusce et lorem metus. Fusce pellentesque sed lacus at facilisis. Suspendisse in.',
-//       status: 'Rejected' as StatusType,
-//       image: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5b/Radiohead_-_No_Surprises_%28CD1%29.jpg/220px-Radiohead_-_No_Surprises_%28CD1%29.jpg',
-//       comment: 'bad',
-//       student_id: 'P0001' ,
-//       timeSubmitted: new Date('2024-03-27T12:00:00Z')
-//     },
-//     {
-//       id: 2,
-//       title: 'There could be hell below, below',
-//       summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae mauris eu ex tincidunt scelerisque vel et risus. Fusce et lorem metus. Fusce pellentesque sed lacus at facilisis. Suspendisse in.',
-//       status: 'Overdue' as StatusType,
-//       image: 'https://i.discogs.com/DV7a-pnwsxi06Ci9Fxyy8pKjWWvDgQAR9RrLE7gOMgo/rs:fit/g:sm/q:90/h:600/w:594/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU2ODk0/ODAtMTM5OTk5NzU2/OC0yMDQ0LmpwZWc.jpeg',
-//       comment: 'bad',
-//       student_id: 'CS001',
-//       timeSubmitted: new Date('2024-03-27T12:00:00Z')
-//     },
-//     {
-//       id: 3,
-//       title: 'Mother Earth is pregnant for the third time',
-//       summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae mauris eu ex tincidunt scelerisque vel et risus. Fusce et lorem metus. Fusce pellentesque sed lacus at facilisis. Suspendisse in.',
-//       status: 'Waiting' as StatusType,
-//       image: 'https://vinylcoverart.com/media/album-covers/3065/funkadelic-maggot-brain.jpg',
-//       comment: '',
-//       student_id: 'CS002',
-//       timeSubmitted: new Date('2024-03-27T12:00:00Z')
-//     }
-//   ];
-
-//   const publishedArticles = [
-//     {
-//         id: 4,
-//         title: "'Cause I'm as free as a bird now",
-//         summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae mauris eu ex tincidunt scelerisque vel et risus. Fusce et lorem metus. Fusce pellentesque sed lacus at facilisis. Suspendisse in.',
-//         status: 'Published' as StatusType,
-//         image: 'https://i.scdn.co/image/ab67616d0000b273128450651c9f0442780d8eb8',
-//         comment: 'good',
-//         student_id: 'P0001',
-//         timeSubmitted: new Date('2024-03-27T12:00:00Z')
-//     },
-//     {
-//         id: 5,
-//         title: 'Sectoral heterochromia',
-//         summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae mauris eu ex tincidunt scelerisque vel et risus. Fusce et lorem metus. Fusce pellentesque sed lacus at facilisis. Suspendisse in.',
-//         status: 'Published' as StatusType,
-//         image: 'https://i1.sndcdn.com/artworks-000157282441-rmtn0q-t500x500.jpg',
-//         comment: 'good',
-//         student_id: 'CS001',
-//         timeSubmitted: new Date('2024-03-27T12:00:00Z')
-//     }
-// ];
-
-
 function MemberTable() {
-  const [isAscending, setIsAscending] = useState(true);
   const [articles, setArticles] = useState<Article[]>([]);
   const [entries, setEntries] = useState<Entry[]>([]);
   const [users, setUsers] = useState<User[]>([]);
   const [faculties, setFaculties] = useState<Faculty[]>([]);
   const [selectedMember, setSelectedMember] = useState<User | null>(null);
   const accessToken = localStorage.getItem("access_token");
-  const [value, setValue] = useState("");
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const toast = useToast();
   const url = 'http://localhost:3001/'
   const boxShadowColor = useColorModeValue('0px 2px 12px rgba(130,148,116,0.8)', '0px 2px 12px rgba(130,148,116,0.8)');
-  const toggleSortOrder = () => {
-    setIsAscending(!isAscending);
-  };
-  
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Function to filter users based on the search query
+  const filteredUsers = users.filter(user =>
+    user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.username.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    user.email.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+
   const handleMemberSelect = (user: User) => {
     setSelectedMember(user);
   };
@@ -313,15 +215,6 @@ function MemberTable() {
     fetchEntries()
     fetchArticles()
   }, []);
-  
-  const StatusButton : React.FC<{ status: StatusType }> = ({ status }) => {
-    let color = 'gray';
-    if (status === 'Waiting') color = '#426B1F';
-    if (status === 'Published') color = '#426B1F';
-    if (status === 'Rejected') color = '#6B1F1F';
-    if (status === 'Overdue') color = '#383838';
-    return <Tag fontSize="lg" fontWeight='bold' width='140px' height='50px' display='flex' alignItems='center' justifyContent='center' borderRadius="full" variant="solid" bg={color} color='white'>{status}</Tag>
-  }
 
   const [selectedComment, setSelectedComment] = useState<Comment[]>([]); 
   const handleClick = async (articleId: string| undefined) => {
@@ -493,112 +386,108 @@ function MemberTable() {
 
   return (
     <Box flex="1" bgGradient="linear(to-t, #e1f5dd, white)" p={5}>
-      <Flex justify="space-between" align="center">
-        <Heading as="h2" fontSize="6xl" mb={10} textColor="#83AD5F">Account Monitor</Heading>
-        <InputGroup maxWidth="300px" mb={10}>
-          <InputLeftElement pointerEvents="none">
-            <FaSearch />
-          </InputLeftElement>
-          <Input placeholder="Search an account" />
-        </InputGroup>
-      </Flex>
-      <Box bg="white" borderRadius="lg" p={8} overflowX="auto" boxShadow={boxShadowColor}>
-        <Flex gap={4} >
-          <Select placeholder="Role" boxShadow={boxShadowColor} width={40}>
-            <option value="student">Student</option>
-            <option value="marketingCoordinator">Marketing Coordinator</option>
-          </Select>
-          <Select placeholder="Sort by" boxShadow={boxShadowColor} width={40}>
-            <option value="name">Name</option>
-            <option value="id">ID</option>
-            <option value="email">Email</option>
-            <option value="role">Role</option>
-            <option value="faculty">Faculty</option>
-          </Select>
-          <Button
-            rightIcon={isAscending ? <Icon as={FaSortAmountUp} /> : <Icon as={FaSortAmountDown} />}
-            variant="outline"
-            onClick={toggleSortOrder}
-            boxShadow={boxShadowColor}
-          >
-            {isAscending ? 'Ascending' : 'Descending'}
-          </Button>
-        </Flex>
-        <Divider my={10} borderColor="#426B1F" width='100%'/>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th fontSize="3xl">ID</Th>
-              <Th fontSize="3xl">Name</Th>
-              <Th fontSize="3xl">Username</Th>
-              <Th fontSize="3xl">Email</Th>
-              <Th fontSize="3xl">Role</Th>
-              <Th fontSize="3xl">Faculty</Th>
-            </Tr>
-          </Thead>
-          <Divider my={4} borderColor="#fff"/>
-          <Tbody>
-            {users.map((user) => (
-              <Tr
-                bg="rgba(137, 188, 93, 0.2)"
-                key={user._id}
-                _hover={{color: '#fff', bg: 'rgba(73,133,23,1)', boxShadow: {boxShadowColor}, transform: 'translateY(-2px)', zIndex: 2}}
-                transition="background-color 0.2s, box-shadow 0.2s, transform 0.2s"
-                position='relative'
-                onClick={() => handleMemberSelect(user)} // Call handleMemberSelect on click
-              >
-                <Td>{user._id}</Td>
-                <Td>{user.name}</Td>
-                <Td>{user.username}</Td>
-                <Td>{user.email}</Td>
-                <Td>{user.roles}</Td>
-                <Td>{findFacultyName(user.faculty_id)}</Td>
-                <Divider my={4} borderColor="#426B1F" width='100%'/>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </Box>
-  
-      <Modal isOpen={selectedMember !== null} onClose={ handleCloseModal}  size='6xl' isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader fontSize='4xl' color='#426b1f'>{selectedMember?.roles.includes('marketingCoordinator')
-            ? `${selectedMember?.name}'s Pending Articles`
-            : `${selectedMember?.name}'s Articles`}
-          </ModalHeader>
-          <ModalCloseButton />
-          <Divider my={2} borderColor="#426B1F" width='100%'/>
-          <ModalBody>
-          {selectedMember?.roles.includes('marketingCoordinator') ? (
-            <MarketingCoordinatorModal
-              selectedUser={selectedMember}
-              selectedArticles={articles}
-              handleClick={handleClick}
-            />
-          ) : (
-            <StudentModal
-              selectedMember={selectedMember}
-              selectedArticles={articles.filter(article => article.student_id === selectedMember?._id)}
-              handleClick={handleClick}
-            />
-          )}
-          </ModalBody>
-        </ModalContent>
-      </Modal>  
+      {showError && (
+        <Alert status="error" mt={4}>
+          <AlertIcon />
+          {errorMessage}
+          <Link href='/login' ml={10}>
+            <Text fontStyle='italic'>Go to the Login Page</Text>
+          </Link>
+        </Alert>
+      )}
+      {userRole.includes('marketingManager') && (
+        <>
+          <Flex justify="space-between" align="center">
+            <Heading as="h2" fontSize="6xl" mb={10} textColor="#83AD5F">Account Monitor</Heading>
+            <InputGroup maxWidth="300px" mb={10}>
+              <InputLeftElement pointerEvents="none">
+                <FaSearch />
+              </InputLeftElement>
+              <Input
+                placeholder="Search a topic"
+                _placeholder={{ color: "gray.500" }}
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />            
+            </InputGroup>
+          </Flex>
+          <Box bg="white" borderRadius="lg" p={8} overflowX="auto" boxShadow={boxShadowColor}>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th fontSize="3xl">ID</Th>
+                  <Th fontSize="3xl">Name</Th>
+                  <Th fontSize="3xl">Username</Th>
+                  <Th fontSize="3xl">Email</Th>
+                  <Th fontSize="3xl">Role</Th>
+                  <Th fontSize="3xl">Faculty</Th>
+                </Tr>
+              </Thead>
+              <Divider my={4} borderColor="#fff"/>
+              <Tbody>
+                {filteredUsers.map((user) => (
+                  <Tr
+                    bg="rgba(137, 188, 93, 0.2)"
+                    key={user._id}
+                    _hover={{color: '#fff', bg: 'rgba(73,133,23,1)', boxShadow: {boxShadowColor}, transform: 'translateY(-2px)', zIndex: 2}}
+                    transition="background-color 0.2s, box-shadow 0.2s, transform 0.2s"
+                    position='relative'
+                    onClick={() => handleMemberSelect(user)} // Call handleMemberSelect on click
+                  >
+                    <Td>{user._id}</Td>
+                    <Td>{user.name}</Td>
+                    <Td>{user.username}</Td>
+                    <Td>{user.email}</Td>
+                    <Td>{user.roles}</Td>
+                    <Td>{findFacultyName(user.faculty_id)}</Td>
+                    <Divider my={4} borderColor="#426B1F" width='100%'/>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
+      
+          <Modal isOpen={selectedMember !== null} onClose={ handleCloseModal}  size='6xl' isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader fontSize='4xl' color='#426b1f'>{selectedMember?.roles.includes('marketingCoordinator')
+                ? `${selectedMember?.name}'s Pending Articles`
+                : `${selectedMember?.name}'s Articles`}
+              </ModalHeader>
+              <ModalCloseButton />
+              <Divider my={2} borderColor="#426B1F" width='100%'/>
+              <ModalBody>
+              {selectedMember?.roles.includes('marketingCoordinator') ? (
+                <MarketingCoordinatorModal
+                  selectedUser={selectedMember}
+                  selectedArticles={articles}
+                  handleClick={handleClick}
+                />
+              ) : (
+                <StudentModal
+                  selectedMember={selectedMember}
+                  selectedArticles={articles.filter(article => article.student_id === selectedMember?._id)}
+                  handleClick={handleClick}
+                />
+              )}
+              </ModalBody>
+            </ModalContent>
+          </Modal>  
 
-      <Modal isOpen={selectedComment.length !== 0} onClose={() => setSelectedComment([])} size="md" isCentered>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader fontSize='3xl' color='#426b1f'>Article Comment</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            {selectedComment!.map(comment => (
-              <Text fontSize='xl' m='5'>{comment.text}</Text>
-            ))}
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+          <Modal isOpen={selectedComment.length !== 0} onClose={() => setSelectedComment([])} size="md" isCentered>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader fontSize='3xl' color='#426b1f'>Article Comment</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                {selectedComment!.map(comment => (
+                  <Text fontSize='xl' m='5'>{comment.text}</Text>
+                ))}
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </>
+        )}
     </Box>
     )
 }
